7629dd856b876ef720c4b04942a408c71824900c
Choi-HyeongSeok <154064687+HyeongSeok-Choi@users.noreply.github.com>
2026-01-22 07:47:52 +0900

    naver login
    네이버 로그인 구현

--- src/common/auth/page/NaverRedirecPage.tsx
+++ src/common/auth/page/NaverRedirecPage.tsx
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { fetchLogin } from "../api/Auth";
 import { useAuthStore } from "../../../store";
 import { useNavigate } from "react-router";
+import { Cookies } from "react-cookie";
 
 const NaverRedirectPage: React.FC = () => {
+  const cookies = new Cookies();
+
+  const accessToken = cookies.get("access_token");
+
+  const [message, setMessage] = useState<string>("네이버 로그인중.....");
+
+  const navigate = useNavigate();
+
+  const { login } = useAuthStore();
+
+  useEffect(() => {
+    localStorage.setItem("accessToken", accessToken);
+    setMessage("네이버 로그인 성공");
+    navigate("/dashboard");
+    login();
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-50 overflow-x-hidden">
       {/*  본문 */}
       <main className="flex-grow flex items-center justify-center py-10 px-4">
-        <h1>네이버 로그인중....</h1>
+        <h1>{message}</h1>
       </main>
     </div>
   );
